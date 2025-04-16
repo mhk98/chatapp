a1ba5987c146f89564d7051d214cb71b2a2c853d
mhk98 <mohsinkabirseo@gmail.com>
2025-04-16 18:44:18 +0600

    'latest'

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,8 +1,22 @@
-import React from "react"
+import queryString from "query-string";
+import React, { useEffect } from "react"
+import { useLocation } from "react-router-dom";
+import io from "socket.io-client"
 
+let socket;
 const Chat = () => {
 
-    return <div className="chat">Chat page</div>
+    const {search} = useLocation()
+    const {name, room} = queryString.parse(search)
+
+    useEffect(() => {
+      socket = io("http://localhost:5000")
+    }, [])
+
+
+    return (
+        <div className="chat">Chat page</div>
+    )
 };
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const http = require("http");
+const socketIO = require("socket.io");
+const cors = require("cors");
+
+const port = 5000;
+const app = express();
+
+// app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
+
+const httpServer = http.createServer(app);
+const io = socketIO(httpServer);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id)
+  })
+});
+
+httpServer.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
